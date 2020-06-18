264fd91d2a51a884e46e068651eed679f08b4658
Margaret Chan <mchan20822@gmail.com>
2020-06-18 00:10:17 +0000

    Remove side effects from helper methods

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -23,6 +23,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public final class FindMeetingQuery {
@@ -31,6 +32,14 @@ public final class FindMeetingQuery {
      * Given all known events and a new event request, find all possible time ranges to schedule 
      * the requested event where all attendees (including optional) can attend. If no such time
      * range exists, return one where all mandatory attendees can attend.
+     *
+     * Detailed cases:
+     * 0. If request has duration greater than a day, return zero slots.
+     * 1. if no required and no optional attendees, return all day.
+     * 2. If all are optional (no required), treat all optional as if they were required.
+     * 3. If has required and no optional, return slots that try to match all required.
+     * 4. If has both required and optional, try to match all attendees. 
+     *    If none exist, return slots to match required only.
      */ 
     public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
         long duration = request.getDuration();
@@ -39,21 +48,26 @@ public final class FindMeetingQuery {
         Collection<String> requiredAttendees = request.getAttendees();
         Collection<String> optionalAttendees = request.getOptionalAttendees();
 
+        // Case 0
         if (duration > TimeRange.WHOLE_DAY.duration()) {
             return new ArrayList<>();
         }
 
         if (!requiredAttendees.isEmpty()) {
+            // Case 3 + 4
             requiredAttendeeTimes = findFreeTimes(events, requiredAttendees, duration);
         } else if (!optionalAttendees.isEmpty()) {
+            // Case 2
             return findFreeTimes(events, optionalAttendees, duration);
         } else {
+            // Case 1
             return Arrays.asList(TimeRange.WHOLE_DAY);
         }
         
-        optionalAttendeeTimes = checkOptionalAttendees(events, requiredAttendeeTimes, request);
+        // In Case 3, this collection is gaurenteed to be empty
+        optionalAttendeeTimes = filterForOptionalAttendees(events, requiredAttendeeTimes, request);
 
-        return (optionalAttendeeTimes.isEmpty() ? requiredAttendeeTimes : optionalAttendeeTimes);
+        return optionalAttendeeTimes.isEmpty() ? requiredAttendeeTimes : optionalAttendeeTimes;
     }
 
     /** 
@@ -66,58 +80,65 @@ public final class FindMeetingQuery {
         List<Event> eventsList = new ArrayList<>(events);
 
         Collection<TimeRange> possibleTimes = new ArrayList<>();
+        Optional<TimeRange> timeOptional;
 
-        filterAndSortEvents(eventsList, attendees);
+        List<Event> sortedEvents = filterAndSortEvents(eventsList, attendees);
 
-        if (eventsList.isEmpty()) {
+        if (sortedEvents.isEmpty()) {
             return Arrays.asList(TimeRange.WHOLE_DAY);
-        } else {
-            // Check for time before first event of day
-            sufficientTime(TimeRange.START_OF_DAY, eventsList.get(0).getWhen().start(), duration, 
-                           possibleTimes, false);
         }
 
-        for (int i = 0; i < eventsList.size(); i++) {
-            Event currEvent = eventsList.get(i);
+        // Check for time before first event of day
+        timeOptional = hasSufficientTime(TimeRange.START_OF_DAY, sortedEvents.get(0).getWhen().start(), 
+                                         duration, /*inclusive=*/false);
+        timeOptional.ifPresent(timeRange -> possibleTimes.add(timeRange));
+
+        for (int i = 0; i < sortedEvents.size(); i++) {
+            Event currEvent = sortedEvents.get(i);
             TimeRange currEventTime = currEvent.getWhen();
             int currEventEnd = currEventTime.end();
             
-            if (i != eventsList.size() - 1) {
-                TimeRange nextEventTime = eventsList.get(i + 1).getWhen();
+            if (i != sortedEvents.size() - 1) {
+                TimeRange nextEventTime = sortedEvents.get(i + 1).getWhen();
                 int nextEventStart = nextEventTime.start();
 
                 if (currEventTime.overlaps(nextEventTime)) {
                     // Check for nested events
                     if (currEventTime.contains(nextEventTime)) {
-                        eventsList.set(i + 1, currEvent);
+                        sortedEvents.set(i + 1, currEvent);
                     }
                 } else {
-                    sufficientTime(currEventEnd, nextEventStart, duration, possibleTimes, false);
+                    timeOptional = hasSufficientTime(currEventEnd, nextEventStart, duration, 
+                                                     /*inclusive=*/false);
+                    timeOptional.ifPresent(timeRange -> possibleTimes.add(timeRange));
                 }
             } else {
                 // Check for time after last event of day
-                sufficientTime(currEventEnd, TimeRange.END_OF_DAY, duration, possibleTimes, true);
+                timeOptional = hasSufficientTime(currEventEnd, TimeRange.END_OF_DAY, duration, 
+                                                 /*inclusive=*/true);
+                timeOptional.ifPresent(timeRange -> possibleTimes.add(timeRange));
             }
         }
         return possibleTimes;
     }
 
     /** 
-     * Checks for sufficient meeting time between startTime and endTime.
-     * If there is enough time, add a new TimeRange to the possibleTimes list.
+     * Checks for sufficient meeting time between startTime and endTime. If there is, 
+     * return Optional of new TimeRange.
      */
-    private void sufficientTime (int startTime, int endTime, long duration, 
-                                 Collection<TimeRange> possibleTimes, boolean inclusive) {
+    private Optional<TimeRange> hasSufficientTime (int startTime, int endTime, long duration, 
+                                                   boolean inclusive) {
         int freeTime = endTime - startTime;
         if (freeTime >= duration) {
-            possibleTimes.add(TimeRange.fromStartEnd(startTime, endTime, inclusive));
+            return Optional.of(TimeRange.fromStartEnd(startTime, endTime, inclusive));
         }
+        return Optional.empty();
     }
 
     /** 
-     * Find the timeOptions which all optional attendees can attend.
+     * Find the TimeRanges in timeOptions which all optional attendees can attend.
      */ 
-    private Collection<TimeRange> checkOptionalAttendees (Collection<Event> events, 
+    private Collection<TimeRange> filterForOptionalAttendees (Collection<Event> events, 
                                 Collection<TimeRange> timeOptions, MeetingRequest request) {
         // Instantiate events as ArrayList in order to use comparator in sorting
         List<Event> eventsList = new ArrayList<>(events);
@@ -125,9 +146,9 @@ public final class FindMeetingQuery {
         // Make a copy to preserve original
         Collection<TimeRange> possibleTimes = new ArrayList<>(timeOptions);
 
-        filterAndSortEvents(eventsList, request.getOptionalAttendees());
+        List<Event> sortedEvents = filterAndSortEvents(eventsList, request.getOptionalAttendees());
 
-        for (Event event : eventsList) {
+        for (Event event : sortedEvents) {
             Iterator<TimeRange> timeRangeIterator = possibleTimes.iterator();
             while (timeRangeIterator.hasNext()) {
 
@@ -144,17 +165,21 @@ public final class FindMeetingQuery {
      * Filter eventsList and keep events which contain any of the requested attendees.
      * Sort eventsList by start time with earliest appearing first. 
      */ 
-    private void filterAndSortEvents (List<Event> eventsList, Collection<String> attendees) {
+    private List<Event> filterAndSortEvents (List<Event> eventsList, Collection<String> attendees) {
+        List<Event> filteredSortedEvents = new ArrayList<>(eventsList);
+
         // Remove events which are irrelevant to the request 
-        eventsList.removeIf(e -> (
+        filteredSortedEvents.removeIf(e -> (
             e.getWhen().duration() <= 0 || Collections.disjoint(e.getAttendees(), attendees)));
 
         // Sort events by start time 
-        Collections.sort(eventsList, new Comparator<Event>() {
+        Collections.sort(filteredSortedEvents, new Comparator<Event>() {
             public int compare (Event e1, Event e2) {
                 return TimeRange.ORDER_BY_START.compare(e1.getWhen(), e2.getWhen());
             }
         });
+
+        return filteredSortedEvents;
     }
 
 }
